98daf917040886ce9990dbec21fcd6b77821c057
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-07-15 08:44:23 -0500

    Validación

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -9,12 +9,20 @@ class RepositoryController extends Controller
 {
     public function store(Request $request)
     {
+        $request->validate([
+            'url' => 'required',
+            'description' => 'required',
+        ]);
         $request->user()->repositories()->create($request->all());
         return redirect()->route('repositories.index');
     }
 
     public function update(Request $request, Repository $repository)
     {
+        $request->validate([
+            'url' => 'required',
+            'description' => 'required',
+        ]);
         $repository->update($request->all());
         return redirect()->route('repositories.edit', $repository);
     }
