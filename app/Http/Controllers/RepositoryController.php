f023ee46c9cf57b54b62487c8f9ddf4021e9200f
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-07-21 08:11:07 -0500

    Validación de datos

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RepositoryRequest;
 use App\Models\Repository;
 use Illuminate\Http\Request;
 
@@ -19,22 +20,14 @@ class RepositoryController extends Controller
         return view('repositories.create');
     }
 
-    public function store(Request $request)
+    public function store(RepositoryRequest $request)
     {
-        $request->validate([
-            'url' => 'required',
-            'description' => 'required',
-        ]);
         $request->user()->repositories()->create($request->all());
         return redirect()->route('repositories.index');
     }
 
-    public function update(Request $request, Repository $repository)
+    public function update(RepositoryRequest $request, Repository $repository)
     {
-        $request->validate([
-            'url' => 'required',
-            'description' => 'required',
-        ]);
 
         if ($request->user()->id != $repository->user_id) {
             abort(403);
